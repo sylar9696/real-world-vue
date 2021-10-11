f42a38b581022ab6c1fe2506283e41f045463d0e
Alessandro Lodigiani <46444351+sylar9696@users.noreply.github.com>
2021-10-11 13:25:35 +0200

    Flash messages

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import { createApp } from 'vue'
+import { createApp, reactive } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//Global store dove salvare variabili utili per ogni componente, la prima che utilizzerò è flashMessages
+const GStore = reactive({ flashMessages: '' })
+
 createApp(App)
   .use(store)
   .use(router)
+  .provide('GStore', GStore)
   .mount('#app')
--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -6,10 +6,20 @@
 <script>
 export default {
   props: ['event'],
+  inject: ['GStore'],
   methods: {
     register() {
       //Call to api
       //If registered then redirect to event details
+
+      this.GStore.flashMessage =
+        'You are successfully registered for ' + this.event.title
+
+      //Pulire il messaggio dopo 3 sec
+      setTimeout(() => {
+        this.GStore.flashMessage = ''
+      }, 3000)
+
       this.$router.push({
         name: 'EventDetails'
       })
